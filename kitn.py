d008b8dd9bbb6eb43425a27e36dcd43f36872d0d
Stephen musk <78314574+bmurder@users.noreply.github.com>
2021-06-23 11:24:45 -0300

    Killing in the name
    Hahaha

---
+++ kitn.py
@@ -0,0 +1,18 @@
+import os
+import webbrowser
+import random 
+
+ini = input('verifique se o arquivo esta na sua area de trabalho, se sim diga qualquer coisa pra continuar: ')
+
+while True:
+  print('killing in the name')
+  webbrowser.open('https://google.com')
+  ran = random.randint(10,55)
+  os.system(f"mkdir {ran}")
+  
+  webbrowser.open('https://media.discordapp.net/attachments/615598200010571818/834939804016967740/SmokeTime.gif')
+  
+  ran2 = random.randint(56,102)
+  os.system(f"mkdir {ran2}")
+ 
+ 
